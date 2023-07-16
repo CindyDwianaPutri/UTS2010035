cfc438262fffb7bf6b50707cc647b66b0f0e8261
Cindy Dwiana Putri <cindydwianaputric@gmail.com>
2023-07-16 07:13:16 -0700

    Initial commit

---
+++ app/src/main/java/com/example/uts2010035/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.uts2010035
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+    }
+}
---
+++ app/src/main/java/com/example/uts2010035/aa.kt
@@ -0,0 +1,18 @@
+package com.example.uts2010035
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.Toast
+
+class aa : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bb)
+        }
+
+    }
+
+
+
